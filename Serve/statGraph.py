38bbf33d0dc6f06e54ac302719f3a655c608d344
Patrick Lorio <plorio@buxo.(none)>
2013-05-12 12:47:45 -0700

    made progress on statGraph api

--- Serve/statGraph.py
+++ Serve/statGraph.py
@@ -64,6 +64,49 @@ def order_nodes(to_define, order=0):
 		order_nodes(next_to_define, order + 1)
 
 def build_steps(nodes):
+	high_order = 0
+	steps = {}
+
+	for node_id in nodes:
+		node = nodes[node_id]
+		if node.order == -1: continue
+		if str(node.order) not in steps:
+			steps[str(node.order)] = []
+		steps[str(node.order)].append(node)
+		if node.order > high_order:
+			high_order = node.order
+
+	res = []
+	for x in range(0, high_order + 1):
+		res.append(steps[str(x)])	
+
+	return res
+
+def get_copy_node(node, output):
+	copy = Node(0, None)
+	copy.inputs = [
+		{
+			'node':node,
+			'output':output
+		}
+	]
+
+	return copy
+
+def fill_steps(steps):
+	for step in range(0, len(steps)):
+		step_nodes = steps[step]
+		for node in step_nodes:
+			for inp in node.inputs:
+				if not inp['node'] in steps[step + 1]:
+					copy_node = get_copy_node(inp['node'], inp['output'])
+					steps[step + 1].append(copy_node)
+
+
+def connect_steps(steps):
+	pass
+
+def serialize_steps(steps):
 	pass
 
 if __name__ == '__main__':
@@ -74,6 +117,11 @@ if __name__ == '__main__':
 
 	nodes = get_nodes(data)
 	order_nodes([nodes[data['end']]])
+	steps = build_steps(nodes)
+	fill_steps(steps)
+	connect_steps(steps)
 
-	for node in nodes:
-		print '%s - %s' % (node, nodes[node].order)
+	# print steps
+
+	# for node in nodes:
+	# 	print '%s - %s' % (node, nodes[node].order)
